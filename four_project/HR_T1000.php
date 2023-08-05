41bdbcec7820631a36937e9f139f4fe04334a2cb
Ruslan <115550042+169408@users.noreply.github.com>
2023-08-05 17:16:00 +0300

    Add files via upload

---
+++ four_project/HR_T1000.php
@@ -0,0 +1,9 @@
+<?php
+
+class HR_T1000 {
+    public $admonition = 0;
+
+    public function count_admonition() {
+        $this->admonition++;
+    }
+}
---
+++ four_project/T1001.php
@@ -0,0 +1,9 @@
+<?php
+
+class T1001 {
+    public $praise = 0;
+
+    public function count_praise() {
+        $this->praise++;
+    }
+}
