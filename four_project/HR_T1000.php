427dd8809876595a77f248ffc30b68df933bacc2
Ruslan <115550042+169408@users.noreply.github.com>
2023-08-09 15:43:19 +0300

    Delete four_project directory

--- four_project/HR_T1000.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class HR_T1000 {
-    public $admonition = 0;
-
-    public function count_admonition() {
-        $this->admonition++;
-    }
-}
--- four_project/T1001.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class T1001 {
-    public $praise = 0;
-
-    public function count_praise() {
-        $this->praise++;
-    }
-}
