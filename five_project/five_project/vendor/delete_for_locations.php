823298327daed8537197d73bafebdd5e86d21b12
Ruslan <115550042+169408@users.noreply.github.com>
2023-08-15 19:47:49 +0300

    Delete five_project/five_project directory

--- five_project/five_project/vendor/delete_for_locations.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once "../config/connect.php";
-
-$id = $_GET["id"];
-
-mysqli_query($connect, "DELETE FROM locations WHERE location_id=$id");
-header("Location: ../index.php");
--- five_project/five_project/vendor/delete_for_players.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once "../config/connect.php";
-
-$id = $_GET["id"];
-
-mysqli_query($connect, "DELETE FROM players WHERE id_player=$id");
-header("Location: ../index.php");
